73d1265bb99a3709cd83ffdec8f2f2dc1872ad26
Jesus S <jsalazar2010@gmail.com>
2023-05-31 19:26:54 -0600

    add navbar and search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Footer from './components/Footer';
-import Contact from './components/Contact';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Header />
       <Login />
-      <Contact />
+      <Search />
       <Footer />
     </div>
   );
