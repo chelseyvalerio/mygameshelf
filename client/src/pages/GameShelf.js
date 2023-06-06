e75f3bcffe825d9357dfb51c3f202ffe21ace88b
Jesus S <jsalazar2010@gmail.com>
2023-06-05 22:26:38 -0600

    adding login page to work

--- client/src/pages/GameShelf.js
+++ client/src/pages/GameShelf.js
@@ -4,10 +4,11 @@ import { selectedGame } from './Search';
 export default function gameShelf() {
   return (
     <div>
+      <section className="all-game-cards">
       <h1 id="my-work">My Game Shelf</h1>
       <p>Welcome to our Board Game Selection App! Are you a board game enthusiast looking for an easy way to discover and save your favorite games? Look no further! </p>
       <p>Our app provides a user-friendly interface that allows you to browse through a vast collection of board games sourced from an API. Once you find a game you love, simply add it to your personal collection for future reference. </p>
-      <section className="all-game-cards">
+      
         <div className="game-card">
           {selectedGame && (
             <article className="game-card">
