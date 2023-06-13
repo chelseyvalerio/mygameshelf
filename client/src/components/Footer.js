86c9874b02e346acac529e5d4507ffb6dddf6e68
Jesus S <jsalazar2010@gmail.com>
2023-06-12 20:25:58 -0600

    trying to add install btn functionality for PWA

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function Modal({ onClose }) {
+    return (
+        <div className='modal'>
+            <p>App installed!</p>
+            <button onClick={onClose}>Close</button>
+        </div>
+    );
+}
 
 function Footer() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleInstallButtonClick = () => {
+        setIsModalOpen(true);
+    };
+
     return (
         <footer>
-            <button className="btn-small" id="buttonInstall" role="button">Install App</button>
+            <button onClick={handleInstallButtonClick} className="btn-small" id="buttonInstall" role="button">Install App</button>
             <h4>&copy; {new Date().getFullYear()} My Game Shelf</h4>
+            {isModalOpen && ReactDOM.createPortal(<Modal onClose={() => setIsModalOpen(false)} />, document.getElementById('modal-root'))}
         </footer>
     );
 }
